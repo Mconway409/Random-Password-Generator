1fe0a5e725ec406f2ffc8c3aac6247c52ddef7d7
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2020-11-14 17:21:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var number = '0123456789';
-var symbol = '!@#$%^&*=_-';
-var lower = 'abcdefghijklmnopqrstuvwxyz';
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var selections = {
+numbers: '0123456789',
+symbols: '!@#$%^&*=_-',
+lower: 'abcdefghijklmnopqrstuvwxyz',
+upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+};
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password").value;
   var characters = document.querySelector("#characters").checked
   var lowerBox = document.querySelector("#lower").checked
   var upperBox = document.querySelector("#upper").checked
-  var numberBox = document.querySelector("#number").checked
-  var symbolBox = document.querySelector("#symbol").checked
-   
-    }
-  
-    passwordText.value = password; 
-for (var i=0; 1 > 8, i <= 128; i++) {
+  var numberBox = document.querySelector("#numbers").checked
+  var symbolBox = document.querySelector("#symbols").checked
+
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
-    (numberBox.checked) ? number : ' ';
-    (symbolBox.checked) ? symbol : ' ';
-    (lowerBox.checked) ? lower : ' ';
-    (upperBox.checked) ? upper : ' ';
-    (lowerBox.checked) && (upperBox.checked) ? lower += upper : ' ';
-    (lowerBox.checked) && (numberBox.checked) ? lower += number : ' ';
-    (lowerBox.checked) && (symbolBox.checked) ? lower += symbol : ' ';
-    (upperBox.checked) && (numberBox.checked) ? upper += number : ' ';
-    (upperBox.checked) && (symbolBox.checked) ? upper += symbol : ' ';
-    (numberBox.checked) && (symbolBox.checked) ? numberr += symbol : ' ';
-    (lowerBox.checked) && (upperBox.checked) && (numberBox.checked) ? lower += upper += number : ' ';
-    (lowerBox.checked) && (upperBox.checked) && (symbolBox.checked) ? lower += upper += symbol : ' ';
-    (lowerBox.checked) && (upperBox.checked) && (numberBox.checked) && (symbolBox.checked) ? lower += upper += number += symbol : ' ';
+    var length = +characters.value;
+    var checkedLower = lower.checked;
+    var checkedUpper = upper.checked;
+    var checkedNumbers = numbers.checked;
+    var checkedSymbols = symbols.checked;
+
 }
 
-for (var i =0; i <10; i++) {
-  var pswrd = Math.floor(Math.random()) * lower.length)
-}
+function writePassword(numbers, symbols, lower, upper, length){
+  var string = "";
+  var password = "";
 
-function password {
-  
-}  
+  var choices = {
+    numbers: numbers,
+    symbols: symbols,
+    lower: lower,
+    upper: upper,
+  };
+
+  for(i=0;i<Object.keys(choices).length;i++){
+    string += (Object.value(choices)[i]) ? selections[Object.keys(choices)[i]] : "";
+  }
+
+  if(string != "" && length > 0){
+    for(i=0;i<length;i++){
+      password += string[Math.floor(math.random() * string.length)];
+    }
+
+    document.querySelector('passwordText]').value = password;
+
+  }
+  else {
+    document.querySelector('passwordText]').value = "Please choose criteria";
+
+  }
+}
